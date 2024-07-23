8b5be33e49a77ea1d4fc2de7603bf3a7dcb6ba21
Joshua Garcia <jgarc374@uic.edu>
2024-07-22 19:02:30 -0500

    Updated first_ui
    Cleaned up code changed button functionality added text description

--- first_ui.py
+++ first_ui.py
@@ -3,100 +3,130 @@ from PIL import Image
 from microaneurysms import *
 from microaneurysmdetection import *
 from bvsegment import *
-import os
-import cv2
-import numpy as np
 
-def click_button():        
-    st.session_state.clicked = True
+
+# Background color of app
+page_bg_img = """
+<style>
+[data-testid="stAppViewContainer"]{
+    background: linear-gradient(#EAE1E1, #EAE1E1);
+}
+</style>
+"""
+st.markdown(page_bg_img, unsafe_allow_html=True)
+
+# Function to handle button click
+def handle_button_click(button_id,img,cap,img_count):
+    if uploaded_file is not None and st.session_state.buttons[button_id] != uploaded_file:
+        st.session_state.images.append((img, cap))
+        st.session_state.buttons[button_id] = uploaded_file
+        img_count += 1
 
 if __name__ == '__main__':
     # Title
     st.header("Diabetic Retinopathy Segmentation Assistant",divider='gray')
+    # Short description on diabetic retinopathy
+    st.markdown("""
+                ***Diabetic retinopathy is a diabetes complication affecting the eyes. 
+                High blood sugar levels damage the blood vessels in the retina, leading 
+                to vision problems. It can occur in anyone with type 1 or type 2 diabetes 
+                and can result in blindness if untreated. Diabetic retinopathy can lead to 
+                vision loss if not detected early. Regular eye exams and good control of 
+                blood sugar, blood pressure, and cholesterol are essential in preventing 
+                and managing this condition.***
+                """)
 
     # Image uploader 
     uploaded_file = st.file_uploader("Upload Image Here:",type=["jpg","jpeg","png"],help="Select an image to upload for analysis")
     uploaded_image = None
+    img_count = 0
 
+    # Initialize session state for keeping track of images and button states 
+    # (to prevent images from dissapearing or being displayed twice)
+    if 'images' not in st.session_state:
+        st.session_state.images = []
+    if 'buttons' not in st.session_state:
+        st.session_state.buttons = {1: None, 2: None, 3: None, 4: None}
+
+    # Check if an image was uploaded
     if uploaded_file is not None:
         # Convert the uploaded file to an OpenCV image
         file_bytes = np.asarray(bytearray(uploaded_file.read()), dtype=np.uint8)
         uploaded_image = cv2.imdecode(file_bytes, 1)
+    # If empty reset the session states for images and buttons.
+    else:
+        st.session_state.buttons = {1: None, 2: None, 3: None, 4: None}
+        st.session_state.images = []
+        
+    # # Display original image 
+    # bl1,img_org,bl2 = st.columns([2,1,2.5])
     
+
+    # Create variables for output analyzed images 
     microaneurysmsdet = None
     microaneurysms = None
     bvsegment = None
-
-    # Button state control
-    if 'clicked' not in st.session_state:
-        st.session_state.clicked = False
-
-
-    # Upload Image to server for analysis
-    bl1, upl, bl2 = st.columns([1,3,1])
-    with upl:
-        st.button("Upload",use_container_width=True, on_click=click_button)
-        if st.session_state.clicked: 
-
-            # Process the images to extract microaneurysms
-            if uploaded_image is not None:
-                microaneurysms = extract_microaneurysms(uploaded_image)
-                microaneurysmsdet = extract_microaneurysmsdet(uploaded_image)
-                bvsegment = extract_bv(uploaded_image)
-                
-                # Convert processed images to a format that can be displayed by Streamlit
-                microaneurysms = Image.fromarray(microaneurysms)
-                microaneurysmsdet = Image.fromarray(microaneurysmsdet)
-                bvsegment = Image.fromarray(bvsegment)
-                st.session_state.clicked = False
-            # else:
-            #   st.write("Please upload image")
-    
-    # Display original eye image
-    bl1,img_org,bl2 = st.columns([2,1,2.5])
+    soft_exudates = None
+    hard_exudates = None
+     
+    # Microaneurysms and Soft Exudate Buttons
+    col1,img_org,col2= st.columns([1,1,1],vertical_alignment="bottom") 
     with img_org:
-        if uploaded_file is not None:
-            st.image(uploaded_file, caption="Original Eye Image", width=200)
+            if uploaded_file is not None:
+                st.image(uploaded_file, caption="Original Eye Image", use_column_width=True)
 
-    # Display analyzed eye images
-    img_mic,bl1,img_bvseg,bl2 = st.columns([1,1,1,0.5])
-    with img_mic:
-        if microaneurysms is not None:
-            st.image(microaneurysms, caption="Microaneurysms found in eye",width=300)
-    with img_bvseg:
-        if microaneurysmsdet is not None:
-            st.image(bvsegment, caption="BV Segmentation of eye", width=300)
-           
+    if col1.button("Microaneurysms",use_container_width=True,help="Uploads image for microaneurysm detection") and uploaded_file is not None:
+        microaneurysms = extract_microaneurysms(uploaded_image)
+        microaneurysms = Image.fromarray(microaneurysms)
+        caption = "Displaying microaneurysms in eye"
+        handle_button_click(1, microaneurysms, caption,img_count)
 
-    # DELETE EXTRA BUTTONS
-    # Display buttons for diagnostic categories
-    col1, b1, col2 = st.columns([2,3,2])
+    if col2.button("Soft Exudates",use_container_width=True,help="Uploads image for soft exudate detection") and uploaded_file is not None:
+        soft_exudates = extract_microaneurysmsdet(uploaded_image)
+        soft_exudates = Image.fromarray(soft_exudates)
+        caption = "Displaying soft exudates found in eye"
+        handle_button_click(2, soft_exudates, caption,img_count)
+
+    # Bvsegment and Hard Exudate Buttons 
+    col3, bl1,col4 = st.columns([1,1,1],vertical_alignment="center")
     
-    # Buttons Microaneurysm and Soft Exudates functionality 
-    with col1:
-        # Displays image analyzed with microaneursysms algorithm 
-        if st.button("Microaneurysms",use_container_width=True):
-           
-            if microaneurysms is not None:
-                st.image(microaneurysms, caption="Microaneurysms found in eye", use_column_width=True)
+    if col3.button("bvsegment",use_container_width=True,help="Uploads image for blood vessel segmentation") and uploaded_file is not None:
+        bvsegment = extract_bv(uploaded_image)
+        bvsegment = Image.fromarray(bvsegment)
+        caption = "Displaying blood vessels in eye"
+        handle_button_click(3, bvsegment, caption,img_count)
 
-        # Displays image analyzed with soft exudates algorithm
-        if st.button("Soft Exudates",use_container_width=True):
+    if col4.button("Hard Exudates",use_container_width=True,help="Uploads image for hard exudates detection") and uploaded_file is not None:
+        hard_exudates = extract_microaneurysmsdet(uploaded_image)
+        hard_exudates = Image.fromarray(hard_exudates)
+        caption = "Displaying hard exudates found in eye"
+        handle_button_click(4, hard_exudates, caption,img_count)
+    
+    st.subheader("Segmentation Results")
+    anl_img1, bl1, anl_img2 = st.columns([0.4,0.1,0.4],vertical_alignment="center",gap="large")
 
-            if microaneurysmsdet is not None:
-                st.image(microaneurysmsdet, caption="Soft Exudates found in eye", use_column_width=True)
+    # Display all images in two columns
+    if uploaded_file is not None and st.session_state.images:
+        for idx, (img, cap) in enumerate(st.session_state.images):
+            if idx % 2 == 0:
+                with anl_img1:
+                    st.image(img, caption=cap,use_column_width=True)
+            else:
+                with anl_img2:
+                    st.image(img, caption=cap, use_column_width=True)
+    
+    # Short descriptions on microaneurysms, blood vessels, hard exudates, and soft exudates.
+    st.markdown("""
+                Blood Vessels: In the eye, blood vessels are critical for supplying the retina 
+                with oxygen and nutrients. 
 
-    # Buttons Hard Exudates and optical disk functionality
-    with col2:
-        # Displays image analyzed with hard exudates algorithm
-        if st.button("BV Segmentation",use_container_width=True):
- 
-            if bvsegment is not None:
-                st.image(bvsegment, caption="BV Segmentation of eye", use_column_width=True)
-        
-        # Displays image analyzed with optical disk algorithm
-        if st.button("Hard Exudates",use_container_width=True):
-            
-            if microaneurysmsdet is not None:
-                st.image(microaneurysmsdet, caption="Hard Exudates found in eye", use_column_width=True)
+                Microaneurysms: Small bulges in the blood vessels of the retina, 
+                caused by weakened capillary walls, and are an early sign of diabetic retinopathy.
+
+                Hard Exudates: Yellowish deposits of lipids and proteins that leak 
+                from damaged blood vessels in the retina, often seen in diabetic retinopathy.
+
+                Soft Exudates: Also known as cotton wool spots, are fluffy white patches 
+                on the retina caused by nerve fiber swelling due to inadequate blood supply.
+                """)
 
